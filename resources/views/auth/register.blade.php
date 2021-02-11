055d42b2a8fb6d9eec134fd397010c465dc01fa8
FatoomMFayad <fatoomfayyad@gmail.com>
2021-02-11 18:35:18 +0200

    link register li to register page using named route

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="flex justify-center">
-    <div class="w-6/12 bg-white p-6 rounded-lg">
+    <div class="w-4/12 bg-white p-6 rounded-lg">
         Register
     </div>
 </div>
